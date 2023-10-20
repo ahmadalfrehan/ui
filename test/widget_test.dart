4aa0033979460d0c75aa6e9bd4fa39f2689efc70
ahmad-frehan <ahmad-frehan@futurecode-co.com>
2023-10-20 10:55:33 +0300

    init

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,29 +12,43 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  TestWidgetsFlutterBinding
-      .ensureInitialized(); // Ensure Flutter bindings are initialized
+  TestWidgetsFlutterBinding.ensureInitialized();
   setUp(() {
-    // Mock network images to prevent actual network requests
     TestWidgetsFlutterBinding.ensureInitialized();
     HttpOverrides.global = null;
     TestWidgetsFlutterBinding.ensureInitialized();
   });
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
-
     expect(find.text("Hi Lay!"), findsOneWidget);
     expect(find.byIcon(Icons.info), findsOneWidget);
-
     expect(find.text("20% of tasks completed"), findsOneWidget);
-    // await tester.tap(find.byIcon(Icons.add));
+
+    expect(find.text("App design for uper"), findsOneWidget);
+    final textWidget = find.text("App design for uper");
+    final positionedWidget = find.byType(Positioned);
+    final containerWithImage = find.byWidgetPredicate((widget) =>
+        widget is Container &&
+        widget.decoration is BoxDecoration &&
+        (widget.decoration as BoxDecoration).border ==
+            Border.all(color: Colors.white) &&
+        (widget.decoration as BoxDecoration).shape == BoxShape.circle &&
+        (widget.decoration as BoxDecoration).image ==
+            const DecorationImage(
+                image: NetworkImage(
+                    "https://ahmadalfrehan.com/assets/assets/images/logo.jpg")));
     final grayContainer = find.byWidgetPredicate((widget) =>
         widget is Container &&
         widget.decoration is BoxDecoration &&
         (widget.decoration as BoxDecoration).borderRadius ==
             BorderRadius.circular(20) &&
         (widget.decoration as BoxDecoration).color == Colors.grey[300]);
+    final hexContainer = find.byWidgetPredicate((widget) =>
+        widget is Container &&
+        widget.decoration is BoxDecoration &&
+        (widget.decoration as BoxDecoration).borderRadius ==
+            BorderRadius.circular(50) &&
+        (widget.decoration as BoxDecoration).color == const Color(0xFF6143e1));
     final greenContainer = find.byWidgetPredicate((widget) =>
         widget is Container &&
         widget.decoration is BoxDecoration &&
@@ -42,7 +56,19 @@ void main() {
             BorderRadius.circular(20) &&
         (widget.decoration as BoxDecoration).color == Colors.greenAccent);
     expect(greenContainer, findsOneWidget);
+    expect(positionedWidget, findsOneWidget);
+    expect(hexContainer, findsOneWidget);
     expect(grayContainer, findsOneWidget);
+    expect(containerWithImage, findsWidgets);
+    expect((tester.widget(hexContainer) as Container).padding,
+        equals(const EdgeInsets.all(20)));
+    expect(
+      (tester.widget(textWidget) as Text).style,
+      equals(
+        const TextStyle(
+            color: Colors.white, fontSize: 20, fontWeight: FontWeight.w800),
+      ),
+    );
 
     await tester.pumpWidget(const MyApp());
 
