05176575727ee6be0f0ca8fb875aedb0c8e2912b
ahmad-frehan <ahmad-frehan@futurecode-co.com>
2023-11-20 10:47:03 +0300

    init

---
+++ lib/home/another.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+
+class Another extends StatelessWidget {
+  const Another({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF38393d),
+        elevation: 0,
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        color: const Color(0xFF38393d),
+        child: Column(
+          children: [
+            _row(),
+            const SizedBox(height: 20),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.white54),
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Row(
+                children: [
+                  Flexible(
+                    child: TextFormField(
+                        decoration: const InputDecoration(
+                      prefixIcon: Icon(Icons.search, color: Colors.white54),
+                      label: Text(
+                        "Search Food ...",
+                        style: TextStyle(color: Colors.white54, fontSize: 17),
+                      ),
+                    )),
+                  ),
+                  Container(
+                    height: 40,
+                    width: 1.5,
+                    color: Colors.white54,
+                  ),
+                  const SizedBox(width: 15),
+                  const Icon(
+                    Icons.filter_list,
+                    color: Colors.white54,
+                  ),
+                  const SizedBox(width: 15),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                for (var i = 0; i < 3; i++) setTh(),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+setTh() {
+  return Stack(
+    alignment: Alignment.centerLeft,
+    children: [
+      Container(
+        width: 123,
+        height: 60,
+        padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50),
+          border: Border.all(color: Colors.white54),
+        ),
+        child: const Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text("Pizza",style: TextStyle(color: Colors.white54),),
+          ],
+        ),
+      ),
+      Container(
+        width: 75,
+        height: 75,
+        decoration: BoxDecoration(
+          color: const Color(0xFF38393d),
+          shape: BoxShape.circle,
+          image: const DecorationImage(
+            image: NetworkImage('https://ahmadalfrehan.com/assets/assets/images/logo.jpg')
+          ),
+          border: Border.all(color: Colors.white54),
+        ),
+
+      )
+    ],
+  );
+}
+
+_row() {
+  return Row(
+    children: [
+      const Column(
+        children: [
+          Text(
+            "Your Location",
+            style: TextStyle(color: Colors.grey),
+          ),
+          Row(
+            children: [
+              Icon(
+                Icons.ac_unit,
+                color: Colors.white,
+              ),
+              Text(
+                " Multan",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            ],
+          ),
+        ],
+      ),
+      const Spacer(),
+      Container(
+        width: 30,
+        height: 30,
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          shape: BoxShape.circle,
+        ),
+        child: const Icon(Icons.notifications),
+      ),
+    ],
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'home/home-screen.dart';
-
 void main() {
   runApp(const MyApp());
 }
@@ -13,7 +11,87 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: HeroAnimation(),
     );
+  }
+}
+
+class HeroAnimation extends StatelessWidget {
+  const HeroAnimation({super.key});
+
+  Widget build(BuildContext context) {
+    var timeDilation = 5.0;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Basic Hero Animation'),
+      ),
+      body: Center(
+        child: PhotoHero(
+          photo: 'https://ahmadalfrehan.com/assets/assets/images/logo.jpg',
+          width: 300.0,
+          onTap: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute<void>(builder: (context) {
+              return Scaffold(
+                appBar: AppBar(
+                  title: const Text('Flippers Page'),
+                ),
+                body: Container(
+                  color: Colors.lightBlueAccent,
+                  padding: const EdgeInsets.all(16),
+                  alignment: Alignment.topLeft,
+                  child: PhotoHero(
+                    photo:
+                        'https://ahmadalfrehan.com/assets/assets/images/logo.jpg',
+                    width: 100.0,
+                    onTap: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ),
+              );
+            }));
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class PhotoHero extends StatelessWidget {
+  const PhotoHero({
+    super.key,
+    required this.photo,
+    this.onTap,
+    required this.width,
+  });
+
+  final String photo;
+  final VoidCallback? onTap;
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      child: Hero(
+        tag: photo,
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: () {},
+            child: Image.network(
+              photo,
+              fit: BoxFit.contain,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  static RectTween _createRectTween(Rect? begin, Rect? end) {
+    return MaterialRectCenterArcTween(begin: begin, end: end);
   }
 }
